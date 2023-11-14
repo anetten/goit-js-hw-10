97cec71100b95f7271e185ce6214afad57259879
anetten <anet.zvekova@gmail.com>
2023-11-14 20:45:44 +0200

    cat2

--- src/cat-api.js
+++ src/cat-api.js
@@ -1,9 +1,8 @@
 export function fetchBreeds() {
   const BASE_URL = 'https://api.thecatapi.com/v1';
   const END_POINT = '/breeds';
-  // const PARAMS = `breeds`;
-  const url = BASE_URL + END_POINT;
-  // + PARAMS;
+  const PARAMS = `breeds`;
+  const url = BASE_URL + END_POINT + PARAMS;
   const options = {
     headers: {
       'x-api-key':
